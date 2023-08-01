843b6d42c3bd9f36c504e5903292460e9b0f744e
sfilipovski <sergejfilipovski@gmail.com>
2023-08-01 22:07:43 +0200

    Added user validation

--- SecurityApi.Models/Models/UserModel.cs
+++ SecurityApi.Models/Models/UserModel.cs
@@ -8,10 +8,16 @@ namespace SecurityApi.Model.Models
     public class UserModel
     {
         [Required]
+        [MinLength(2)]
+        [MaxLength(50)]
         public string FirstName { get; set; }
         [Required]
+        [MinLength(2)]
+        [MaxLength(50)]
         public string LastName { get; set; }
         [Required]
+        [MinLength(2)]
+        [MaxLength(50)]
         public string Username { get; set; }
         [Required]
         public string Role { get; set; }
